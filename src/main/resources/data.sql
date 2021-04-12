8d590e7c388cb182bfb969993bc09bd66a77d979
Bago <ahmadovba@gmail.com>
2021-04-13 03:24:08 +0400

    Docker configs done and liquibase setup configured

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
-
-
 INSERT INTO users (name,surname,email, password) VALUES ('name1','sname1','@mail1', '#pass1');
 INSERT INTO users (name,surname,email, password) VALUES ('name2','sname2','@mail2', '#pass2');
 INSERT INTO users (name,surname,email, password) VALUES ('name3','sname3','@mail3', '#pass3');
 INSERT INTO users (name,surname,email, password) VALUES ('name4','sname4','@mail4', '#pass4');
 INSERT INTO users (name,surname,email, password) VALUES ('name5','sname5','@mail5', '#pass5');
+
+
+# make spring.datasource.initialization-mode=always to enable.
